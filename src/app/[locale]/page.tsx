57a07be6dd4a556803a990116ecae13e1251f019
Wamiq <wamiqzahid319@gmail.com>
2025-05-22 06:53:24 +0500

    Initial Deployement Issues fixed 3

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,25 +1,19 @@
 import HomePage from "./pages/home";
-// import AboutPage from "@/pages/about";
-// import ServicesPage from "@/pages/services";
-// import ContactPage from "@/pages/contact";
 
-type Props = {
-  searchParams?: Record<string, string | string[] | undefined>;
+type PageProps = {
+  params: { locale: string };
+  searchParams?: Promise<Record<string, string | string[] | undefined>>;
 };
 
-export default function LocaleRouter({ searchParams }: Props) {
-  const pageParam = searchParams?.page;
+export default async function LocaleRouter({ searchParams }: PageProps) {
+  const resolvedSearchParams = searchParams ? await searchParams : undefined;
+  const pageParam = resolvedSearchParams?.page;
   const page = Array.isArray(pageParam) ? pageParam[0] : pageParam ?? "home";
 
   switch (page) {
     case "home":
       return <HomePage />;
-    // case "about":
-    //   return <AboutPage />;
-    // case "services":
-    //   return <ServicesPage />;
-    // case "contact":
-    //   return <ContactPage />;
+
     default:
       return <HomePage />;
   }
