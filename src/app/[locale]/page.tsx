ead12f36c1cda03861a0d087dab9bddd0005044a
Wamiq <wamiqzahid319@gmail.com>
2025-05-23 02:37:31 +0500

    Initiail Deployment  Fixed  4

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,20 +1,5 @@
 import HomePage from "./pages/home";
 
-type PageProps = {
-  params: { locale: string };
-  searchParams?: Promise<Record<string, string | string[] | undefined>>;
-};
-
-export default async function LocaleRouter({ searchParams }: PageProps) {
-  const resolvedSearchParams = searchParams ? await searchParams : undefined;
-  const pageParam = resolvedSearchParams?.page;
-  const page = Array.isArray(pageParam) ? pageParam[0] : pageParam ?? "home";
-
-  switch (page) {
-    case "home":
-      return <HomePage />;
-
-    default:
-      return <HomePage />;
-  }
+export default function Page() {
+  return <HomePage />;
 }
